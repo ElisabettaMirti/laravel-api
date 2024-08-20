9d9ea9cc7f6957247aa86990c20a383e0f9f1b65
ElisabettaMirti <belmirti@gmail.com>
2024-08-20 18:22:41 +0200

    creato controller per i project nell cartella api e create route per index e show

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index(){
+
+        $projects = project::all();
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+
+    public function show(Project $project){
+
+    }
+}
